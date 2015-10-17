cd505621333b0e8b18b9ecaeb4d6eaadfb3e5499
qza <kokovic.zoran@gmail.com>
2015-10-17 13:07:02 +0200

    purchase-ticket.feature runner

---
+++ src/test/java/bdd/example/ticket/PurchaseTicketFeature.java
@@ -0,0 +1,11 @@
+package bdd.example.ticket;
+
+import org.junit.runner.RunWith;
+
+import bdd.FeaturesBase;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+public class PurchaseTicketFeature extends FeaturesBase {
+
+}
